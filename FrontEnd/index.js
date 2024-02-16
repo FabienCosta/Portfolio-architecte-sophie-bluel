048fc8be0f4b27725db0a94225af373ef263ce91
Fabien Costa <fabiencosta94800@gmail.com>
2024-02-17 00:34:54 +0100

    array for works and add in html

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,4 +1,19 @@
-const worksInfo =  fetch('http://localhost:5678/api/works').then(res => res.json());
-console.log(worksInfo);
-const lala = worksInfo.then(data => data[5].title);
-console.log(lala);
+await fetch('http://localhost:5678/api/works')
+  .then(response => response.json())
+  .then(data => {
+    // En supposant que les données sont un objet, convertissez-le en tableau
+    const dataArray = Object.keys(data).map(key => data[key]);
+    const galleryItem = document.querySelector('.gallery');
+    for (let i = 0; i < dataArray.length; i++) {
+
+        const image = document.createElement('img');
+        const title = document.createElement('figcaption');
+        image.src = dataArray[i].imageUrl;
+        image.alt = dataArray[i].title;
+        title.textContent = dataArray[i].title;
+        galleryItem.appendChild(image);
+        galleryItem.appendChild(title);
+    }
+  })
+  .catch(error => console.error('Erreur:', error));
+

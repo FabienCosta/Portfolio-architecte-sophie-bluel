917b38e10ef82010938d8c9f17ac61ccd8dbc20f
Fabien Costa <fabiencosta94800@gmail.com>
2024-02-20 22:21:27 +0100

    add filter button

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,48 +1,125 @@
 // Description: This file is used to display the gallery of the projects and to filter them by category
 const response = await fetch("http://localhost:5678/api/works");
 const data = await response.json();
-console.log(data);
-// await fetch("http://localhost:5678/api/works")
-//   .then((response) => response.json())
-//   .then((data) => {
-//     let dataArray = Object.keys(data).map((key) => data[key]);
-//     let galleryItem = document.querySelector(".gallery");
-//     for (let i = 0; i < dataArray.length; i++) {
-//       let figure = document.createElement("figure"); // Create a new figure element
-//       let image = document.createElement("img");
-//       let title = document.createElement("figcaption");
-//       image.src = dataArray[i].imageUrl;
-//       image.alt = dataArray[i].title;
-//       title.textContent = dataArray[i].title;
-//       figure.appendChild(image); // Append the img and figcaption to the figure
-//       figure.appendChild(title);
-//       galleryItem.appendChild(figure); // Append the figure to the galleryItem
-//     }
-//   })
-//   .catch((error) => console.error("Erreur:", error));
 
-// let sectionId = document.getElementById("projects");
-// const filter = document.createElement("div");
-// filter.classList.add("filter");
-// sectionId.appendChild(filter);
-// const buttonAll = document.createElement("button");
-// buttonAll.classList.add("button_filter");
-// buttonAll.textContent = "Tous";
-// const buttonObjects = document.createElement("button");
-// buttonObjects.classList.add("button_filter");
-// buttonObjects.textContent = "Objets";
-// const buttonAppartment = document.createElement("button");
-// buttonAppartment.classList.add("button_filter");
-// buttonAppartment.textContent = "Appartements";
-// const buttonHotel = document.createElement("button");
-// buttonHotel.classList.add("button_filter");
-// buttonHotel.textContent = "Hotel & Restaurants";
-// filter.appendChild(buttonAll);
-// filter.appendChild(buttonObjects);
-// filter.appendChild(buttonAppartment);
-// filter.appendChild(buttonHotel);
+// ? Global Variables
+let galleryItem = document.querySelector(".gallery");
+let sectionId = document.getElementById("projects");
+const filter = document.createElement("div");
+filter.classList.add("filter");
+sectionId.appendChild(filter);
 
-// buttonObjects.addEventListener("click", function () {
-//   console.log("click");
-// });
+// ? Functions
+function galeryCreation() {
+  for (let i = 0; i < data.length; i++) {
+    let figure = document.createElement("figure");
+    let image = document.createElement("img");
+    let title = document.createElement("figcaption");
+    image.src = data[i].imageUrl;
+    image.alt = data[i].title;
+    title.textContent = data[i].title;
+    figure.appendChild(image);
+    figure.appendChild(title);
+    galleryItem.appendChild(figure);
+  }
+}
 
+function filterAll() {
+  let dataAll = data;
+  const buttonAll = document.createElement("button");
+  buttonAll.classList.add("button_filter");
+  buttonAll.textContent = "Tous";
+  buttonAll.addEventListener("click", function () {
+    galleryItem.innerHTML = "";
+    for (let i = 0; i < dataAll.length; i++) {
+      let figure = document.createElement("figure");
+      let image = document.createElement("img");
+      let title = document.createElement("figcaption");
+      image.src = dataAll[i].imageUrl;
+      image.alt = dataAll[i].title;
+      title.textContent = dataAll[i].title;
+      figure.appendChild(image);
+      figure.appendChild(title);
+      galleryItem.appendChild(figure);
+    }
+  });
+
+  filter.appendChild(buttonAll);
+}
+
+function filterObjects() {
+  let dataObjects = data.filter((data) => data.category.name === "Objets");
+  const buttonObjects = document.createElement("button");
+  buttonObjects.classList.add("button_filter");
+  buttonObjects.textContent = "Objets";
+  buttonObjects.addEventListener("click", function () {
+    galleryItem.innerHTML = "";
+    for (let i = 0; i < dataObjects.length; i++) {
+      let figure = document.createElement("figure");
+      let image = document.createElement("img");
+      let title = document.createElement("figcaption");
+      image.src = dataObjects[i].imageUrl;
+      image.alt = dataObjects[i].title;
+      title.textContent = dataObjects[i].title;
+      figure.appendChild(image);
+      figure.appendChild(title);
+      galleryItem.appendChild(figure);
+    }
+  });
+  filter.appendChild(buttonObjects);
+}
+
+function filterApartment() {
+  let dataApartment = data.filter(
+    (data) => data.category.name === "Appartements"
+  );
+  const buttonApartment = document.createElement("button");
+  buttonApartment.classList.add("button_filter");
+  buttonApartment.textContent = "Appartements";
+  buttonApartment.addEventListener("click", function () {
+    galleryItem.innerHTML = "";
+    for (let i = 0; i < dataApartment.length; i++) {
+      let figure = document.createElement("figure");
+      let image = document.createElement("img");
+      let title = document.createElement("figcaption");
+      image.src = dataApartment[i].imageUrl;
+      image.alt = dataApartment[i].title;
+      title.textContent = dataApartment[i].title;
+      figure.appendChild(image);
+      figure.appendChild(title);
+      galleryItem.appendChild(figure);
+    }
+  });
+  filter.appendChild(buttonApartment);
+}
+
+function filterHotel() {
+  let dataHotel = data.filter(
+    (data) => data.category.name === "Hotels & restaurants"
+  );
+  const buttonHotel = document.createElement("button");
+  buttonHotel.classList.add("button_filter");
+  buttonHotel.textContent = "Hotel & Restaurants";
+  buttonHotel.addEventListener("click", function () {
+    galleryItem.innerHTML = "";
+    for (let i = 0; i < dataHotel.length; i++) {
+      let figure = document.createElement("figure");
+      let image = document.createElement("img");
+      let title = document.createElement("figcaption");
+      image.src = dataHotel[i].imageUrl;
+      image.alt = dataHotel[i].title;
+      title.textContent = dataHotel[i].title;
+      figure.appendChild(image);
+      figure.appendChild(title);
+      galleryItem.appendChild(figure);
+    }
+  });
+  filter.appendChild(buttonHotel);
+}
+
+// ? function call
+galeryCreation();
+filterAll();
+filterObjects();
+filterApartment();
+filterHotel();

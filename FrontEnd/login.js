3c80ca5d74182ebb4b199b627a9c52070f5b24d5
Fabien Costa <fabiencosta94800@gmail.com>
2024-02-22 03:26:26 +0100

    test post with API

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,6 +1,26 @@
-function login(username, password) {
+// ? Variables Globales
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("current-password");
+const loginButton = document.querySelector(".login_button");
 
-}
+// ? evenement sur le bouton de connexion
+const loginForm = document.querySelector(".login_form");
+loginForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
 
-// Example usage
-login("admin", "password");
+  const response = await fetch("http://localhost:5678/api/users/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: "sophie.bluel@test.tld",
+      password: "S0phie",
+    }),
+  });
+  const data = await response.json();
+  const token = data.token;
+  sessionStorage.setItem("token", token);
+  console.log("Token:", token);
+  console.log(data);
+});

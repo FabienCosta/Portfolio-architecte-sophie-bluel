434b1c54f063033cc3e51c6888c0eb1f0bb38d78
Fabien Costa <fabiencosta94800@gmail.com>
2024-02-22 03:42:13 +0100

    login form with input

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -7,20 +7,20 @@ const loginButton = document.querySelector(".login_button");
 const loginForm = document.querySelector(".login_form");
 loginForm.addEventListener("submit", async (e) => {
   e.preventDefault();
-
+const email = emailInput.value;
+const password = passwordInput.value;
   const response = await fetch("http://localhost:5678/api/users/login", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      email: "sophie.bluel@test.tld",
-      password: "S0phie",
+      email: email,
+      password: password,
     }),
   });
   const data = await response.json();
-  const token = data.token;
-  sessionStorage.setItem("token", token);
-  console.log("Token:", token);
+//   const token = data.token;
+//   sessionStorage.setItem("token", token);
   console.log(data);
 });
